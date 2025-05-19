c763d641cb1fa05581d2ab893219813245730fdd
Sen-26 <81495960+Sen-26@users.noreply.github.com>
2025-05-19 19:21:58 -0400

    upload image to server
    sends the image to the server qand recieves trash count

--- Skoup/app/profile.tsx
+++ Skoup/app/profile.tsx
@@ -1,3 +1,12 @@
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+
 export default function Profile() {
     return <View><Text>Profile page</Text></View>;
   }
