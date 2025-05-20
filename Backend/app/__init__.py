51474a51fc1d292c955cbecf6045623a736c2e6d
aadityasah <aaditya.sah365@gmail.com>
2025-05-20 18:23:36 -0400

    avatar stuff + heatmap code

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -13,9 +13,11 @@ def create_app():
     from app.detection.routes import detection_bp
     from app.auth.routes import auth_bp
     from app.users.routes import users_bp
+    # from app.heatmap.routes import heatmap_bp
 
     app.register_blueprint(detection_bp, url_prefix='/detection')
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(users_bp, url_prefix='/users')
+    # app.register_blueprint(heatmap_bp, url_prefix='/heatmap')
 
     return app
